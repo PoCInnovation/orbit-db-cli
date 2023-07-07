13ca61f5ce6e8cb6301749af327b5ac6a61b891b
Xavier Mitault <xavier.mitault@epitech.eu>
2023-07-07 10:11:04 +0200

    docs(commands feed create): more examples + description of force

--- orbit-db-cli/src/commands/feed/create.ts
+++ orbit-db-cli/src/commands/feed/create.ts
@@ -8,21 +8,20 @@ export default class Create extends Command {
   static description = 'Create an feed type database'
 
   static examples = [
-    '<%= config.bin %> <%= command.id %>',
+    '<%= config.bin %> <%= command.id %> --name=myFeedDbLOL',
+    '<%= config.bin %> <%= command.id %> --name=myFeedDbLOL --force',
   ]
 
   static flags = {
     // flag with a value (-n VALUE, --name=VALUE)
     name: Flags.string({ char: 'n', description: 'name of the database', required: true }),
     // flag with no value (-f, --force)
-    force: Flags.boolean({ char: 'f' }),
+    force: Flags.boolean({ char: 'f', description: 'force overwrite if DB already exists' }),
   }
 
   public async run(): Promise<void> {
     const { flags } = await this.parse(Create);
-    this.log(`starting orbitdb...`);
     const orbitdb = await startOrbitDB();
-    this.log(`started orbitdb`);
 
     this.log(`creating database name: ${flags.name} ...`);
     const db = await createDB(orbitdb, flags.name, 'feed', flags.force);

19996d67b09a20182118e4a5b506446817472243
Xavier Mitault <xavier.mitault@epitech.eu>
2023-07-21 09:10:28 +0200

    refactor(lint-fix): run lint fix

--- src/commands/counter/index.ts
+++ src/commands/counter/index.ts
@@ -3,15 +3,11 @@ import { Command } from "@oclif/core";
 export default class Counter extends Command {
   static description = "counter related commands (see examples below)";
 
-  static examples = [
-    "<%= config.bin %> <%= command.id %> --help"
-  ];
+  static examples = ["<%= config.bin %> <%= command.id %> --help"];
 
-  static flags = {
-  };
+  static flags = {};
 
-  static args = {
-  };
+  static args = {};
 
   public async run(): Promise<void> {
     this.error("try 'counter --help' for more information");

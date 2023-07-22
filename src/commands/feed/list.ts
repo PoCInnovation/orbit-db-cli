c89a88e3db46cf3e19eb5b2516c50ab820b31c27
Ylan Garnier <ylan.garnier@epitech.eu>
2023-07-22 20:32:42 +0200

    feat(feed info): command to list the entries of a feed database

--- src/commands/feed/list.ts
+++ src/commands/feed/list.ts
@@ -6,7 +6,7 @@ import { openDB } from "../../utils/open-DB";
 import { resolveDBIdByName } from "../../utils/resolve-DBIdByName";
 import { saveDB } from "../../utils/save-DB";
 
-export default class FeedInfo extends Command {
+export default class FeedList extends Command {
   static description = "Show informations about a feed type database";
 
   static examples: Command.Example[] = [
@@ -16,14 +16,20 @@ export default class FeedInfo extends Command {
   static flags = {
     // flag with a value (-n VALUE, --name=VALUE)
     dbName: Flags.string({
-      char: "n",
+      char: 'n',
       description: "name of the database",
       required: true,
     }),
+
+    // flag with a value (-n VALUE, --name=VALUE)
+    limit: Flags.integer({
+      char: 'l',
+      description: "number of entries you want to query",
+    })
   };
 
   public async run(): Promise<void> {
-    const { flags } = await this.parse(FeedInfo);
+    const { flags } = await this.parse(FeedList);
     const orbitdb = await startOrbitDB(true);
     const dbAdress = await resolveDBIdByName(orbitdb, flags.dbName, "feed");
     const DBExists = await doesDBExists(orbitdb, dbAdress);
@@ -35,15 +41,15 @@ export default class FeedInfo extends Command {
     }
 
     const db = await openDB(orbitdb, dbAdress, "feed")
-    this.log('--- Database informations ---\n')
-    this.log(`Name: ${db.dbname}`)
-    this.log(`Type: ${db._type}`)
-    this.log(`Adress: ${db.address.toString()}`)
-    this.log(`Owner: ${db.id}`)
-    this.log(`Data file: ./${db._cache.path}`)
-    this.log(`Entries: ${db._oplog.length}`)
-    this.log(`Write-Access: ${db.access.write}`)
-
+    const entries = db.iterator({ limit: flags.limit || -1, reverse: true })
+    if (entries.length > 0) {
+      this.log(`--- Database last ${entries.length} entries ---`)
+      entries.forEach((entry: any) => {
+        this.log(`${JSON.stringify(entry.payload.value, null, 2)}`)
+      });
+    } else {
+      this.log(`Database ${db.dbname} is empty`)
+    }
     await saveDB(db);
     await db.close();
     await stopOrbitDB(orbitdb);
